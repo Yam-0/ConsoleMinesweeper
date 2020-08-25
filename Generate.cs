61bb505039850699112bdf8fe9b526c6014b82dd
Yam-0 <tage.akerstrom@gmail.com>
2020-08-25 10:11:03 +0200

    Moving things around

--- Generate.cs
+++ Generate.cs
@@ -4,13 +4,6 @@ class Generate
 {
 	public static string[,] GenerateMap(bool[,] bombMap, MapSize currentMap, bool debugMode)
 	{
-		/*
-		ConsoleMinesweeper.Program program = new ConsoleMinesweeper.Program();
-
-		MapSize currentMap = program.currentMap;
-		bool debugMode = program.debugMode;
-		*/
-
 		//Instantiate new map instance
 		string[,] map = new string[currentMap.x, currentMap.y];
 
@@ -44,5 +37,67 @@ class Generate
 		}
 
 		return map;
+	}
+
+	//Generate a field of mines
+	public static bool[,] GenerateBombMap(MapSize currentMap, bool debugMode, string charOffset)
+	{
+		//Declare 2d array of booleans
+		bool[,] bombMap = new bool[currentMap.x, currentMap.y];
+
+		//Loop over every pixel
+		for (int y = 0; y < currentMap.y; y++)
+		{
+			for (int x = 0; x < currentMap.x; x++)
+			{
+				//50% chance for every square to contain a bomb
+				Random random = new Random();
+				Boolean bomb = false;
+
+				if (random.Next(0, 5) == 0)
+				{
+					bomb = true;
+				}
+				bombMap[x, y] = bomb;
+			}
+		}
+
+		//Show the bombMap to the player if debug mode is on
+		if (debugMode)
+		{
+			Console.Clear();
+
+			Tools.WriteLineWithColor("Debug window", ConsoleColor.Red);
+
+			//Loop through squares and draw bombs
+			for (int y = 0; y < currentMap.y; y++)
+			{
+				Console.WriteLine();
+				for (int x = 0; x < currentMap.x; x++)
+				{
+					//Draw bombs and empty squares
+					if (bombMap[x, y] == true)
+					{
+						Console.Write("*");
+					}
+					else
+					{
+						Console.Write("'");
+					}
+
+					//distance between chars
+					Console.Write(charOffset);
+				}
+			}
+
+			Console.WriteLine();
+			Console.WriteLine();
+			Tools.WriteLineWithColor("Press any button to continue", ConsoleColor.Red);
+			Console.ReadKey(true);
+		}
+
+		//Clear and continue
+		Console.Clear();
+		return bombMap;
 	}
 }
--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace ConsoleMinesweeper
 			//Instance of class for non-static fields
 			Program program = new Program();
 			program.Start();
-			bool[,] bombMap = program.GenerateBombMap();
+			bool[,] bombMap = Generate.GenerateBombMap(program.currentMap, program.debugMode, program.charOffset);
 			string[,] map = Generate.GenerateMap(bombMap, program.currentMap, program.debugMode);
 			program.Loop(map, bombMap);
 
@@ -89,68 +89,6 @@ namespace ConsoleMinesweeper
 			}
 
 			return;
-		}
-
-		//Generate a field of mines
-		bool[,] GenerateBombMap()
-		{
-			//Declare 2d array of booleans
-			bool[,] bombMap = new bool[currentMap.x, currentMap.y];
-
-			//Loop over every pixel
-			for (int y = 0; y < currentMap.y; y++)
-			{
-				for (int x = 0; x < currentMap.x; x++)
-				{
-					//50% chance for every square to contain a bomb
-					Random random = new Random();
-					Boolean bomb = false;
-
-					if (random.Next(0, 5) == 0)
-					{
-						bomb = true;
-					}
-					bombMap[x, y] = bomb;
-				}
-			}
-
-			//Show the bombMap to the player if debug mode is on
-			if (debugMode)
-			{
-				Console.Clear();
-
-				Tools.WriteLineWithColor("Debug window", ConsoleColor.Red);
-
-				//Loop through squares and draw bombs
-				for (int y = 0; y < currentMap.y; y++)
-				{
-					Console.WriteLine();
-					for (int x = 0; x < currentMap.x; x++)
-					{
-						//Draw bombs and empty squares
-						if (bombMap[x, y] == true)
-						{
-							Console.Write("*");
-						}
-						else
-						{
-							Console.Write("'");
-						}
-
-						//distance between chars
-						Console.Write(charOffset);
-					}
-				}
-
-				Console.WriteLine();
-				Console.WriteLine();
-				Tools.WriteLineWithColor("Press any button to continue", ConsoleColor.Red);
-				Console.ReadKey(true);
-			}
-
-			//Clear and continue
-			Console.Clear();
-			return bombMap;
 		}
 
 		void Loop(string[,] map, bool[,] bombMap)
